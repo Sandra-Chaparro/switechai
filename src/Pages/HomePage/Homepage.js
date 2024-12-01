6992f8d678b3947b65d4d605a7143a120396ad9d
Sandra Chaparro <sandra.saldepaz@gmail.com>
2024-11-30 21:47:25 -0600

    hero text style v1

--- src/Pages/HomePage/Homepage.js
+++ src/Pages/HomePage/Homepage.js
@@ -1,8 +1,32 @@
 import React from "react";
+import backgroundImage from "../../Images/heroimg.jpg";
 import "./Homepage.css";
 
 const Homepage = () => {
-  return <div className="homepage">Homepage</div>;
+  return (
+    <div className="homepage">
+      <div className="hero">
+        <div className="hero-image">
+          <img
+            src={backgroundImage}
+            alt="Tech Image"
+            className="w-full h-auto"
+          />
+        </div>
+        <div className="hero-text ">
+          <h1 className=" hero-h1">
+            Custom Software, <br></br>AI-Powered Virtual Assistants, and
+            Seamless Digital Solutions
+          </h1>
+          <p className="hero-p">Automate Your Business Processes.</p>
+          <p className="hero-p">Elevate Your Digital Transformation Today.</p>
+        </div>
+      </div>
+      <div className="services">SERVICES</div>
+      <div className="portfolio">PORTFOLIO</div>
+      <div className="contactus">CONTACT US</div>
+    </div>
+  );
 };
 
 export default Homepage;
